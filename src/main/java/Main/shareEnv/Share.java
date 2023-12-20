90acb1660abea2178b621c9d765ac886b76bce23
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-12-20 08:20:44 +0700

    call api login

---
+++ src/main/java/Main/shareEnv/Share.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main.shareEnv;
+
+/**
+ *
+ * @author HP-PC
+ */
+public class Share {
+    public static String apiURL = "http://10.126.4.82:8881";
+    
+}
---
+++ target/classes/Main/controller/AuthController.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.controller;
+import Main.shareEnv.*;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author HP-PC
+ */
+
+
+public class AuthController {
+
+    boolean apiLogin(String username, String password) {
+        try {
+            // Create a URL object with the API endpoint
+            URL url = new URL(Share.apiURL + "/user/login");
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to POST
+            connection.setRequestMethod("POST");
+
+            // Enable output and set the content type
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            // Create the request body
+            String requestBody = String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password);
+
+            try ( // Write the request body to the output stream
+                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                outputStream.writeBytes(requestBody);
+                outputStream.flush();
+            }
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+
+            StringBuilder response;
+            try ( // Read the response from the API
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+
+            // Parse the response as JSON
+            JSONObject jsonResponse = new JSONObject(response.toString());
+
+            // Access the properties in the response body
+            String message = jsonResponse.getString("message");
+            String id = jsonResponse.getString("id");
+
+            // Print the response
+            System.out.println("Response Code: " + responseCode);
+            System.out.println("Message: " + message);
+            System.out.println("ID: " + id);
+
+            if (responseCode == 200){
+                return true;
+            }
+            
+            // Close the connection
+            connection.disconnect();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+//    boolean apiRegister(String fullname, String username, String pass, String email, String dob, String address, String gender ){
+//        
+//    }
+}
